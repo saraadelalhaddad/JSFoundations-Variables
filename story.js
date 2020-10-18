d22bbcf7c028990b6be8ec73abadd9317531ce02
saraadelalhaddad <sara.alhaddad@colorado.edu>
2020-10-18 20:02:54 +0300

    Added story!

---
+++ story.js
@@ -0,0 +1,5 @@
+let username = "Sara";
+let age = 28;
+let hobby = "Rollerskating";
+
+console.log(`Mr./Mrs. ${username}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`)
